9dd8595b16a49e3388d152afdcc0dc669c76dfb5
Justin Schuetz <jschuetz@gmri.org>
2016-10-24 11:12:53 -0400

    revise port filter script

--- Data Preparation/identify ports sampled every year from 2011-2015 and aggregate by Colburn communities.R
+++ Data Preparation/identify ports sampled every year from 2011-2015 and aggregate by Colburn communities.R
@@ -24,6 +24,8 @@ my.lbs.value <- tbl_df(read.csv("Z:/COCA-conf/Landings/Mills_1982-2015 GAR Landi
   
 write.csv(my.lbs.value, "Z:/COCA-conf/Output/ports sampled every year 2011-2015 with landed lbs and value aggregated.csv", row.names=F)
 
+
+
 ##### read in colburn commiunities and join
 
 colburn <- tbl_df(read.csv("Z:/COCA-conf/Landings/Ports Aggregated.csv"))
