f01eacb4cf24e514938a03c14cd94818ec2b410b
Justin Schuetz <jschuetz@gmri.org>
2016-12-25 15:57:30 -0500

    update ports and communities

--- Data Preparation/identify ports sampled every year from 2011-2015 and aggregate by Colburn communities.R
+++ Data Preparation/identify ports sampled every year from 2011-2015 and aggregate by Colburn communities.R
@@ -1,8 +1,5 @@
-##### identify which ports report landings data every year between 2011 and 2015
-
 library(tidyverse)
 library(stringr)
-
 
 
 ##### identify ports sampled every year 2011-2015
@@ -12,7 +9,6 @@ my.ports <- tbl_df(read.csv("Z:/COCA-conf/Landings/Mills_1982-2015 GAR Landings 
   group_by(STATE, PORT, PORT_CODE) %>%
   summarise(N.YEARS = n()) %>%
   filter(N.YEARS == 5)
-
 
 
 ##### landed lbs and value for ports sampled every year 2011-2015
@@ -30,17 +26,54 @@ write.csv(my.lbs.value, "Z:/COCA-conf/Output/ports sampled every year 2011-2015 
 
 
 
+
+
+
+
 ##### aggregate values based on colburn communities when those are different from ports
+
+# this relaxes the criterion that ports report landings every year from 2011-2015...because they
+# combine with ports that are sampled every year to form colburns' fishing communities
+
+
+my.ports <- tbl_df(read.csv("Z:/COCA-conf/Landings/Mills_1982-2015 GAR Landings combined sheets.csv")) %>%
+  filter(YEAR > 2010, YEAR < 2016, duplicated(str_c(YEAR, STATE, PORT, PORT_CODE)) == FALSE) %>%
+  group_by(STATE, PORT, PORT_CODE) %>%
+  summarise(N.YEARS = n())
+
+
+##### landed lbs and value for ports sampled every year 2011-2015
+
+my.lbs.value <- tbl_df(read.csv("Z:/COCA-conf/Landings/Mills_1982-2015 GAR Landings combined sheets.csv")) %>%
+  filter(YEAR > 2010, YEAR < 2016) %>%
+  semi_join(my.ports) %>%
+  group_by(STATE, PORT, PORT_CODE) %>%
+  summarise(SUM_LANDED_LBS_2011_2015 = sum(LANDED_LBS, na.rm=T), 
+            SUM_VALUE_2011_2015 = sum(VALUE, na.rm=T)) %>%
+  mutate(MEAN_LANDED_LBS_2011_2015 = SUM_LANDED_LBS_2011_2015/5,
+         MEAN_VALUE_2011_2015 = SUM_VALUE_2011_2015/5)
+
 
 colburn <- read_csv("Z:/COCA-conf/Landings/Ports Aggregated.csv")
 names(colburn)[1:2] <- c("PORT", "STATE")
 
+# change port names in colburn data so they match landings data port names
+
+colburn$PORT[colburn$PORT == "SPRUCE HEAD"] <- "SPRUCEHEAD"
+colburn$PORT[colburn$PORT == "SAINT GEORGE"] <- "ST. GEORGE"
+colburn$PORT[colburn$PORT == "GOULDSBORO"] <- "GOLDSBORO"
+
+
 my.lbs.value.c <- left_join(my.lbs.value, colburn) %>%
-  mutate(JGS.COMMUNITY = GEO_NAME_ACS2014)
+  mutate(JGS.COMMUNITY = str_to_upper(GEO_NAME_ACS2014)) %>%
+  mutate(JGS.COMMUNITY = gsub("/", "_", JGS.COMMUNITY, fixed = T)) %>%
+  mutate(JGS.COMMUNITY = gsub(",", "", JGS.COMMUNITY, fixed = T)) %>%
+  mutate(JGS.COMMUNITY = gsub(" ", "_", JGS.COMMUNITY, fixed = T)) %>%
+  mutate(JGS.COMMUNITY = gsub("-", "_", JGS.COMMUNITY, fixed = T)) 
   
 i <- is.na(my.lbs.value.c$JGS.COMMUNITY)
 
-my.lbs.value.c[i, "JGS.COMMUNITY"] <- str_c(my.lbs.value.c$PORT[i], ", ", my.lbs.value.c$STATE[i])
+my.lbs.value.c[i, "JGS.COMMUNITY"] <- str_c(my.lbs.value.c$PORT[i], "_", my.lbs.value.c$STATE[i])
 
 final.communities <- group_by(my.lbs.value.c, JGS.COMMUNITY) %>%
   mutate(MEAN_LANDED_LBS_2011_2015_COMMUNITY = sum(MEAN_LANDED_LBS_2011_2015), 
